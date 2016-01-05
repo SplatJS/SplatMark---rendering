7c29a0bbd719dac6a53f1a828859ba7deb528b12
Eric Lathrop <eric@ericlathrop.com>
2016-01-05 00:56:47 -0500

    Update message

--- src/systems/renderer/draw-stats.js
+++ src/systems/renderer/draw-stats.js
@@ -6,6 +6,6 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 		context.font = "18px helvetica";
 		var spriteCount = game.entities.find("sprite").length;
 		context.fillText("Sprites: " + spriteCount, 20, 35);
-		context.fillText("Press Space to add more", 20, 75);
+		context.fillText("Click or press Space to add more", 20, 75);
 	});
 };
