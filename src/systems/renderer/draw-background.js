abebbf6bcf7d8e5de56ecc4ead2f92342c248cd2
Eric Lathrop <eric@ericlathrop.com>
2016-01-05 00:14:10 -0500

    Rename data->game

--- src/systems/renderer/draw-background.js
+++ src/systems/renderer/draw-background.js
@@ -1,14 +1,10 @@
 "use strict";
 
-
-module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
-
+module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	ecs.add(function(entity, context) { // eslint-disable-line no-unused-vars
 		context.fillStyle = "#242521";
-		context.fillRect(0, 0, data.canvas.width, 120);
+		context.fillRect(0, 0, game.canvas.width, 120);
 		context.fillStyle = "#799DA3";
-		context.fillRect(0, 120, data.canvas.width, data.canvas.height - 120);
-
+		context.fillRect(0, 120, game.canvas.width, game.canvas.height - 120);
 	});
-
 };
--- src/systems/simulation/keep-sprites-on-screen.js
+++ src/systems/simulation/keep-sprites-on-screen.js
@@ -3,32 +3,28 @@
 //var screenShake = require("../../screen-shake");
 
 //var camera = 10;
-module.exports = function(ecs,data) { // eslint-disable-line no-unused-vars
+module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function keepSpritesOnScreen(entity, elapsed) { // eslint-disable-line no-unused-vars
-		data.entities.set(0, "spriteCount", data.entities.find("sprite").length);
-		var size = data.entities.get(entity, "size");
-		var position = data.entities.get(entity, "position");
-		var velocity = data.entities.get(entity, "velocity");
-
-
+		game.entities.set(0, "spriteCount", game.entities.find("sprite").length);
+		var size = game.entities.get(entity, "size");
+		var position = game.entities.get(entity, "position");
+		var velocity = game.entities.get(entity, "velocity");
 
 		if (position.x < 0) {
 			position.x = 0;
 			velocity.x = -velocity.x;
 		}
-		if (position.x + size.width > data.canvas.width) {
-			position.x = data.canvas.width - size.width;
+		if (position.x + size.width > game.canvas.width) {
+			position.x = game.canvas.width - size.width;
 			velocity.x = -velocity.x;
 		}
 		if (position.y < 120) {
 			position.y = 120;
 			velocity.y = -velocity.y;
-
 		}
-		if (position.y + size.height > data.canvas.height) {
-			position.y = data.canvas.height - size.height;
+		if (position.y + size.height > game.canvas.height) {
+			position.y = game.canvas.height - size.height;
 			velocity.y = -velocity.y;
-
 		}
 	},"sprite");
 };
