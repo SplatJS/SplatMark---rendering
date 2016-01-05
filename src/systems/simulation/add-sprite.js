dcfe26a4d2eecf1cdc7f9a41c51298b3351b1180
Eric Lathrop <eric@ericlathrop.com>
2016-01-05 00:55:47 -0500

    Add more sprites faster, and with mouse

--- src/systems/simulation/add-sprite.js
+++ src/systems/simulation/add-sprite.js
@@ -5,10 +5,8 @@ var add = require("../../addSprite");// eslint-disable-line no-unused-vars
 //var camera = 10;
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	ecs.add(function addSprite(elapsed) { // eslint-disable-line no-unused-vars
-		if (game.input.buttonPressed("action")) {
-			for (var i = 0; i < 10; i++) {
-				add(game, "sprite");
-			}
+		if (game.input.button("action") || game.input.mouse.isPressed(0)) {
+			add(game, "sprite");
 		}
 	});
 };
